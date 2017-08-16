c8c9317deaf4715b8d02bc857d614e3c890fabfd
Gyula Krizsma <gyula.krizsma@softfactors.com>
2017-08-16 23:35:36 +0200

    IHomeSanitizer implemented

---
+++ Kowalsky/Controllers/Sanitizers/HomeSanitizer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Kowalsky.Controllers.Dtos;
+using Kowalsky.Models;
+
+namespace Kowalsky.Controllers.Sanitizers
+{
+    public class HomeSanitizer : IHomeSanitizer
+    {
+        public ContactInfo SanitizeContactInfo(ContactInfoDto dto)
+        {
+            if (dto == null)
+            {
+                throw new Exception("ContactInfo is null");
+            }
+
+            if (string.IsNullOrWhiteSpace(dto.Name))
+            {
+                throw new Exception("Name is empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(dto.Email))
+            {
+                throw new Exception("Email is empty");
+            }
+
+            if (!IsValidEmail(dto.Email))
+            {
+                throw new Exception("Email is not valid");
+            }
+
+            if (string.IsNullOrWhiteSpace(dto.Phone))
+            {
+                throw new Exception("Phone is empty");
+            }
+
+            return new ContactInfo(dto.Name, dto.Email, dto.Phone, dto.Comment);
+        }
+
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/1365407/c-sharp-code-to-validate-email-address
+        /// </summary>
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Kowalsky/Controllers/Sanitizers/IHomeSanitizer.cs
@@ -0,0 +1,10 @@
+﻿using Kowalsky.Controllers.Dtos;
+using Kowalsky.Models;
+
+namespace Kowalsky.Controllers.Sanitizers
+{
+    public interface IHomeSanitizer
+    {
+        ContactInfo SanitizeContactInfo(ContactInfoDto dto);
+    }
+}
