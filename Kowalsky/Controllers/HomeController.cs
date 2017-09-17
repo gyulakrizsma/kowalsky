f9392980bec85178532125859dc9fa8d4a0e249e
Gyula Krizsma <gyula.krizsma@softfactors.com>
2017-09-17 15:03:09 +0200

    Content added and _Schedules extra logic applied

--- Kowalsky/Controllers/HomeController.cs
+++ Kowalsky/Controllers/HomeController.cs
@@ -10,26 +10,24 @@ namespace Kowalsky.Controllers
         {
             var schedules = new[]
             {
-                new Schedule(new DateTime(2017, 4, 18), new DateTime(2017, 4, 19), new DateTime(2017, 4, 20), new DateTime(2017, 4, 24), new DateTime(2017, 4, 25), new DateTime(2017, 4, 26), new DateTime(2017, 4, 27)),
-                new Schedule(new DateTime(2017, 5, 15), new DateTime(2017, 5, 17), new DateTime(2017, 5, 18), new DateTime(2017, 5, 22), new DateTime(2017, 5, 24), new DateTime(2017, 5, 25), new DateTime(2017, 5, 29)),
-                new Schedule(new DateTime(2017, 6, 12), new DateTime(2017, 6, 14), new DateTime(2017, 6, 15), new DateTime(2017, 6, 19), new DateTime(2017, 6, 21), new DateTime(2017, 6, 22), new DateTime(2017, 6, 26)),
-                new Schedule(new DateTime(2017, 7, 17), new DateTime(2017, 7, 19), new DateTime(2017, 7, 20), new DateTime(2017, 7, 24), new DateTime(2017, 7, 26), new DateTime(2017, 7, 27), new DateTime(2017, 7, 31)),
+                new Schedule(new DateTime(2017, 9, 18), new DateTime(2017, 9, 20), new DateTime(2017, 9, 21), new DateTime(2017, 9, 25), new DateTime(2017, 9, 27), new DateTime(2017, 9, 28), new DateTime(2017, 10, 02)),
+                new Schedule(new DateTime(2017, 10, 16), new DateTime(2017, 10, 18), new DateTime(2017, 10, 19), new DateTime(2017, 10, 24), new DateTime(2017, 10, 25), new DateTime(2017, 10, 26), new DateTime(2017, 10, 30)),
+                new Schedule(new DateTime(2017, 11, 13), new DateTime(2017, 11, 15), new DateTime(2017, 11, 16), new DateTime(2017, 11, 20), new DateTime(2017, 11, 22), new DateTime(2017, 11, 23), new DateTime(2017, 11, 27)),
             };
 
             var prices = new[]
             {
                 new Price(7000, "Orvosi alkamlassági vizsgálat", ""),
-                new Price(7000, "Egészségügyi tanfolyam", "Test"),
+                new Price(7000, "Egészségügyi tanfolyam", ""),
                 new Price(5500, "Egészségügyi vizsgadíj", ""),
-                new Price(4600, "Elméleti vizsgadíj (kresz teszt)", "Test"),
-                new Price(1100, "Forgalmi vizsgadíj", "")
+                new Price(4600, "Elméleti vizsgadíj (kresz teszt)", ""),
+                new Price(11000, "Forgalmi vizsgadíj", "")
             };
 
             var comments = new[]
             {
-                new Comment("Rendkívül elégedett voltam a szolgáltatásokkal.", "Kovács Dezső", "Tervező mérnök"),
-                new Comment("A krizsmajogsi a legjobb autósiskola a fővárosban. Mindenkinek ajánlom. Gábor egy sokat tudó jól képzett szakember. Örülök, hogy nála tanulhattam meg vezetni", "Kiss Kinga", "Fogászati asszisztens"),
-                new Comment("A E-learning szolgáltatás segítségével sokkal könnyebben tudtam megtanulni a tananyagot. A gyakorlati vezetés pedig magas szinten történt, egy igen jól felszerelt autóval.", "Kedves Ferenc", "Sofőr")
+                new Comment("Szuper volt az elméleti oktatás, amit Krizsma Gábor tartott, külön köszönet az oktatómnak, Császár Viktornak, kismamakánt különösen értékeltem a türelmes és segítőkész hozzáállását.", "Keszthelyiné Simon Viktória", ""),
+                new Comment("Legjobb suli érdekesek az órái, vezetői oktatàsa nyugis Köszönjük K. Gábor", "Trudics Kriszta", "")
             };
 
             var model = new HomeOverviewViewModel(99000, schedules, prices, comments);
