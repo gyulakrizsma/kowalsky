3229372cf9e290649bea0203bf7f65fd9ad56068
Gyula Krizsma <gyula.krizsma@softfactors.com>
2017-10-04 20:03:01 +0200

    typos fixed in EmailTemplateService

--- Kowalsky/Services/EmailTemplateService.cs
+++ Kowalsky/Services/EmailTemplateService.cs
@@ -11,11 +11,11 @@ namespace Kowalsky.Services
 
             var sb = new StringBuilder();
             sb.Append(
-                "<p>Köszönjük, hogy megtisztelt a bizalmával. Rövid időn belül jelentkezni fogunk vagy az alábbi elérhetőségeken:<p>");
+                "<p>Köszönjük, hogy megtisztelt a bizalmával. Rövid időn belül jelentkezni fogunk az alábbi elérhetőségeken:<p>");
 
-            sb.Append($"<p>Email: {contactInfo.Email}</p>");
+            sb.Append($"<p>E-mail cím: {contactInfo.Email}</p>");
             sb.Append($"<p>Telefon: {contactInfo.Phone}</p>");
-            sb.Append("<p>Amennyiben a válaszunkra nem tud várni, kérem hívja az 0630 970 6890-es számot. </p>");
+            sb.Append("<p>Amennyiben a válaszunkra nem tud várni, kérem hívja a 0630 970 6890-es számot. </p>");
             sb.Append("<p>Üdvözlettel,<br />Krizsma Gábor</p>");
 
             return (subject, sb.ToString());
@@ -29,7 +29,7 @@ namespace Kowalsky.Services
             sb.Append("<p>Az alábbi személy jelentkezett a weboldalon keresztül<p>");
             sb.Append($"<p>Név: {contactInfo.Name}</p>");
             sb.Append($"<p>Telefon: {contactInfo.Phone}</p>");
-            sb.Append($"<p>E-mail: {contactInfo.Email}</p>");
+            sb.Append($"<p>E-mail cím: {contactInfo.Email}</p>");
 
             if (!string.IsNullOrWhiteSpace(contactInfo.Comment))
             {
