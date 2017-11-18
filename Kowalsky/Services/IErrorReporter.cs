b157f0436a7839f71dc789c9651365d65fee6b90
Gyula Krizsma <gyula.krizsma@softfactors.com>
2017-11-18 16:43:11 +0100

    SentryErrorReporter implemented and IErrorReporter added

---
+++ Kowalsky/Services/IErrorReporter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kowalsky.Services
+{
+    public interface IErrorReporter
+    {
+        Task CaptureAsync(Exception exception);
+
+        Task CaptureAsync(string message);
+    }
+}
---
+++ Kowalsky/Services/Sentry/SentryErrorReporter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using SharpRaven.Core;
+using SharpRaven.Core.Data;
+
+namespace Kowalsky.Services.Sentry
+{
+    public class SentryErrorReporter : IErrorReporter
+    {
+        private readonly SentryOptions _options;
+
+        public SentryErrorReporter(IOptions<SentryOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public async Task CaptureAsync(Exception exception)
+        {
+            if (!string.IsNullOrWhiteSpace(_options.Dsn))
+            {
+                var client = new RavenClient(_options.Dsn);
+                await client.CaptureAsync(new SentryEvent(exception));
+            }
+        }
+
+        public async Task CaptureAsync(string message)
+        {
+            if (!string.IsNullOrWhiteSpace(_options.Dsn) && !string.IsNullOrWhiteSpace(message))
+            {
+                var client = new RavenClient(_options.Dsn);
+                await client.CaptureAsync(new SentryEvent(message));
+            }
+        }
+    }
+}
